7cc134d7fc96eca2384628cd0f44a6621da7b7c1
Dung Ngok Kenneth Li <119074781+KennethDNKL@users.noreply.github.com>
2023-09-15 17:18:44 -0600

    finish

---
+++ app/StudentInfo.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+export default function StudentInfo() {
+    return (
+        <div>
+            <p>Name: Dung Ngok Kenneth Li</p>
+            <p>Course section: CPRG 306 A</p>
+            <Link href="https://github.com/KennethDNKL/cprg306-assignments.git">https://github.com/KennethDNKL/cprg306-assignments.git</Link>
+        </div>
+    )
+  }
