3b155d3f5607960af846e3de14dfe6dcc251bc57
Dung Ngok Kenneth Li <119074781+KennethDNKL@users.noreply.github.com>
2023-10-09 01:09:32 -0600

    week 5 starts

---
+++ app/week5/page.js
@@ -0,0 +1,11 @@
+import ItemList from './item-list'
+
+export default function page() {
+    return (
+        <main class="bg-slate-950"><div>
+            <h2 class="text-3xl font-bold m-2">Shopping List</h2>
+            <ItemList/>
+            </div>
+            </main>
+    )
+}
