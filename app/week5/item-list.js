fd62f9633b602e6dd11bedb8344e7495f5ed3af2
Dung Ngok Kenneth Li <119074781+KennethDNKL@users.noreply.github.com>
2023-10-23 17:55:52 -0600

    Instructor updated

--- app/week5/item-list.js
+++ app/week5/item-list.js
@@ -1,8 +1,11 @@
-import Item from "./item";
-import React, { useState } from 'react';
-import items from './items.json';
+"use client";
 
-export default function ItemList() {
+import Item from "./item";
+import itemsData from './items.json';
+import React, { useState } from 'react';
+
+
+/**export default function ItemList() {
   const [sortBy, setSortBy] = useState('name');
   const [nameButtonColor, setNameButtonColor] = useState('bg-orange-700');
   const [categoryButtonColor, setCategoryButtonColor] = useState('bg-orange-700');
@@ -54,6 +57,92 @@ export default function ItemList() {
           />
         ))}
       </ul>
+    </div>
+  );
+}*/
+
+export default function ItemList() {
+  const [sortBy, setSortBy] = useState("name");
+  let items = [...itemsData];
+  if (sortBy === "name") {
+    items.sort((a, b) => a.name.localeCompare(b.name));
+  }
+  if (sortBy === "category") {
+    items.sort((a, b) => a.category.localeCompare(b.category));
+  }
+  const groupedItems = items.reduce((groups, item) => {
+    const category = item.category;
+    if (!groups[category]) {
+      groups[category] = [];
+    }
+    groups[category].push(item);
+    groups[category].sort((a, b) => a.name.localeCompare(b.name));
+    return groups;
+  }, {});
+  return (
+    <div className="m-4">
+      <h2 className="text-3xl font-bold m-2">Shopping List</h2>
+      <div>
+        <label htmlFor="sort">Sort by: </label>
+        <button
+          onClick={() => setSortBy("name")}
+          className={
+            sortBy === "name"
+              ? "bg-orange-500 p-1 m-2 w-28"
+              : "bg-orange-700 p-1 m-2 w-28"
+          }
+        >
+          Name
+        </button>
+        <button
+          onClick={() => setSortBy("category")}
+          className={
+            sortBy === "category"
+              ? "bg-orange-500 p-1 m-2 w-28"
+              : "bg-orange-700 p-1 m-2 w-28"
+          }
+        >
+          Category
+        </button>
+        <button
+          onClick={() => setSortBy("group-category")}
+          className={
+            sortBy === "group-category"
+              ? "bg-orange-500 p-1 m-2 w-28"
+              : "bg-orange-700 p-1 m-2 w-28"
+          }
+        >
+          Grouped Category
+        </button>
+        <label
+          className="absolute top-20 left-240  text-gray-600 text-xs italic"
+          htmlFor="group-category"
+        >
+          &larr; &quot;Grouped Category&quot; is an optional extra challenge
+        </label>
+      </div>
+      {sortBy === "group-category" ? (
+        <div>
+          {Object.entries(groupedItems)
+            .sort((a, b) => a[0].localeCompare(b[0])) // sort categories
+            .map(([category, items]) => (
+              <div key={category}>
+                <h3 className="capitalize text-xl">{category}</h3>
+                <ul>
+                  {items.map((item) => (
+                    <Item key={item.id} {...item} />
+                  ))}
+                </ul>
+              </div>
+            ))}
+        </div>
+      ) : (
+        <ul>
+          {items.map((item) => (
+            <Item key={item.id} {...item} />
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
