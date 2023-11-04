8d134405973c54465daade3a5a8495d69ac60392
Dung Ngok Kenneth Li <119074781+KennethDNKL@users.noreply.github.com>
2023-11-04 16:40:03 -0600

    Update page.js

--- app/week8/page.js
+++ app/week8/page.js
@@ -36,7 +36,9 @@ export default function Page() {
           {user && (
                     <p>
                         <Link href="/week8/shopping-list" 
-                        className="px-fit py-fit text-black bg-red-300 rounded hover:bg-brown-700 ">Welcome to my shopping list</Link>
+                        className="px-fit py-fit text-black bg-red-300 rounded hover:bg-brown-700 ">
+                        Welcome to my shopping list
+                        </Link>
                     </p>
                 )}
         </div>
@@ -47,76 +49,4 @@ export default function Page() {
         </Link>
       </main>
     );
-    /**return (
-        <div>
-            <h1>Week 8</h1>
-            <h2>shopping-list</h2>
-            <p>Current user: {user?.displayName}</p>
-
-            <button onClick={handleSignIn}>Sign in with GitHub</button>
-
-            <button onClick={handleSignOut}>Sign out</button>
-
-        
-            <Link href="/week8">week 8</Link> 
-        </div>
-    );*/
 }
-
-
-/**export default function Page()  {
-    const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
-    
-    function handleSignIn() {
-        gitHubSignIn();
-    }
-
-    function handleSignOut(){
-        firebaseSignOut();
-    }
-
-    return(
-        <div>
-            <h1>Week 8</h1>
-            <h2>shopping-list</h2>
-            <p>Current user: {user?.displayName}</p>
-            
-            <button onClick={handleSignIn}>Sign in with GitHub</button>
-
-            
-            <button onClick={handleSignOut}>Sign out</button>
-        </div>
-        
-    )
-
-}*/
-
-/*function ShoppingList() {
-    const { user, firebaseSignOut } = useUserAuth();
-  
-    // Define the URL for the login page
-    const loginPageURL = 'http://localhost:3000/week8/login'; // Replace with the actual URL
-  
-    // Check if the user is authenticated
-    if (!user) {
-      // If the user is not authenticated, you can handle it here
-      // Display a message for unauthenticated users with a link to the login page
-      return (
-        <div>
-          <p>You need to be logged in to access the shopping list.</p>
-          <p>Access the login page: <a href={loginPageURL}>Login Page</a></p>
-        </div>
-      );
-    }
-  
-    // If the user is authenticated, you can render the shopping list content
-    return (
-      <div>
-        <p>Welcome, {user.displayName} ({user.email})</p>
-        {Add your shopping list content here }
-        <button onClick={firebaseSignOut}>Sign out</button>
-      </div>
-    );
-  }
-  
-  export default ShoppingList;*/
